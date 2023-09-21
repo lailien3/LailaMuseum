a7230128c8d50fc7aae259edfbf8599476bd6d72
Laila Aleissa <laila@Lailas-MacBook-Air.local>
2023-09-21 16:31:02 +0100

    modified components and added images and links

--- src/App.js
+++ src/App.js
@@ -4,23 +4,24 @@ import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HeroSection from './components/HeroSection';
-
-// import Services from './components/pages/Services';
-// import Products from './components/pages/Products';
-// import SignUp from './components/pages/SignUp';
+import Cards from './components/Cards';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-
         <HeroSection />
+        <Cards/>
+
         <Routes>
           <Route path='/' exact component={Home} />
-          {/* <Route path='/services' component={Services} />
+          <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} /> */}
+          <Route path='/sign-up' component={SignUp} />
         </Routes>
       </Router>
     </>
