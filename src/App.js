68703c469c8a44cf328638d0e8bfd362b01721a7
Laila Aleissa <laila@Lailas-MacBook-Air.local>
2023-09-21 20:30:28 +0100

    added images to links

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HeroSection from './components/HeroSection';
-import Cards from './components/Cards';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
@@ -14,15 +12,14 @@ function App() {
     <>
       <Router>
         <Navbar />
-        <HeroSection />
-        <Cards/>
-
+        
         <Routes>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/services' element={<Services/>} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
         </Routes>
+        
       </Router>
     </>
   );
