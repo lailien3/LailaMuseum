8af812c756a37deaa251164971e933e61154e2da
Laila Aleissa <laila@Lailas-MacBook-Air.local>
2023-10-09 14:38:07 +0100

    modified snake game and added css

--- API/java/repository/PlantRepository.java
+++
@@ -1,9 +0,0 @@
-package lailas-blog.API.java.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import lailas-blog.API.java.model.Plant;
-
-public interface PlantRepository extends JpaRepository<Plant, Long>{
-
-}
